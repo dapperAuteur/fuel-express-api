b241a5df3f9475958f326c0ec11f7a337e4a1f95
Anthony McDonald <a@awews.com>
2018-07-28 15:56:57 -0400

    add transaction routes and model

---
+++ models/transaction.js
@@ -0,0 +1,45 @@
+let mongoose = require('mongoose');
+let User = require('./user');
+
+var transactionSchema = new mongoose.Schema({
+  transactionEvent: {
+    type: String,
+    required: true
+  },
+  account: {
+    type: String,
+    required: true
+  },
+  dateTime: {
+    type: String,
+    required: false
+  },
+  details: {
+    type: String,
+    required: false
+  },
+  amount: {
+    type: Number,
+    default: 0,
+    required: true
+  },
+  currency: {
+    type: String,
+    default: "USD: US Dollars",
+    required: true
+  },
+  tags: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Tag'
+  }],
+  currentUserId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+let Transaction = mongoose.model('Transaction', transactionSchema);
+module.exports = Transaction;
---
+++ routes/transactions.js
@@ -0,0 +1,15 @@
+let express = require('express');
+let router = express.Router();
+let db = require('./../models');
+let helpersTransactions = require('./../helpers/helpers_transactions');
+
+router.route('/')
+  .get(helpersTransactions.getTransactions)
+  .post(helpersTransactions.createTranaction);
+
+router.route('/:transactionId')
+  .get(helpersTransactions.getTransaction)
+  .put(helpersTransactions.updateTransaction)
+  .delete(helpersTransactions.deleteTransaction)
+
+module.exports = router;
